05d80db3f3fa98c02197ac18915884e94d540abd
YutaSugao <mrfeather@YutaSugaonoMacBook.local>
2018-04-18 15:21:48 +0900

    modfied

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
-  q
+
   has_many :items
   has_many :cart_items
   has_many :orders
