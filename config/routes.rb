37ae7f5ab1e636fbcaa19993ba3b27549721357e
sugayu <mygit_yutasugao20171126@yahoo.co.jp>
2018-04-19 07:08:43 +0000

    finish_admins_routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admins do
+    get 'discs/edit'
+  end
+
+  namespace :admins do
+    get 'items/edit'
+  end
+
   get '/admins/' => 'admins#top'
   get '/' => 'items#index', as: 'top'
   get '/thanks' => 'users#thanks'
@@ -16,31 +24,34 @@ Rails.application.routes.draw do
     :passwords => 'users/passwords'
   }
 
-namespace :admins do
-    resources :items, only: [:show, :create] do
-      resources :discs, only: [:show] do
+  namespace :admins do
+    resources :items, only: [:show, :create, :edit, :update] do
+      resources :discs, only: [:show, :edit, :update] do
         resource :songs, only: [:create, :destroy, :update]
+      end
     end
-  end
     resources :artists, only: [:show , :new, :create]
     resources :users, only: [:show, :index, :edit, :update]
-end
-
-namespace :admins do
-  resources :artists, only: [:show] do
-    resource :items, only: [:new]
   end
-end
 
+  namespace :admins do
+    resources :artists, only: [:show] do
+      resource :items, only: [:new]
+    end
+  end
 
-resources :users, only: [:show, :edit, :update] do
+  get 'admins/stock'=> 'admins#stock', as: 'admins_stock'
+
+  get 'admins/status'=> 'admins#status', as: 'admins_status'
+
+  resources :users, only: [:show, :edit, :update] do
     resources :sendings, only: [:create, :edit, :update]
     resources :orders, only: [:new, :create, :edit, :update]
     resources :cart_items, only: [:index]
   end
- resources :artists, only: [:create]
- resources :genres, only: [:show]
- resources :items, only: [:intex, :show, :create]
+  resources :artists, only: [:create]
+  resources :genres, only: [:show]
+  resources :items, only: [:intex, :show, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
