4937b82f3097d9f9d726f9d7d90a17900da7b005
NaganegiWebcamp <esh6gc2@gmail.com>
2018-04-28 04:45:22 +0000

    a

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -5,6 +5,7 @@ before_action :authenticate_admin!
     @artist = @item.artist
     @discs = @item.discs
     @genre = @item.genre
+    @disc = Disc.new
   end
 
   def new
@@ -37,6 +38,30 @@ before_action :authenticate_admin!
     item = Item.find(params[:item_id])
     item.update(item_show_flg: 0)
     redirect_to admins_artist_path(item.artist.id)
+  end
+
+  def itemshow
+    item = Item.find(params[:item_id])
+    item.update(item_show_flg: 1)
+    redirect_to admins_artist_path(item.artist.id)
+  end
+
+  def manage_stock
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admins_orders_path
+
+  #   item = Item.find(params[:id])
+    # 在庫の増減がやりたい人生だった..けどそのためには@sumを保存するカラムが必要になりそう？
+    # メンターさんに聞きます。
+    # @sums = 0
+    # stock = item.stock
+      # if @sums >= 0
+        # item.stock = stock + @sums
+        # redirect_to admins_orders_path
+      # else
+      # item.stock = stock - @sums
+      # end
   end
 
   private
