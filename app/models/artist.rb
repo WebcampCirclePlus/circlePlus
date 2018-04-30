427e3bc40eb33c24ce93fa980d3ca1b98ef87044
NaganegiWebcamp <esh6gc2@gmail.com>
2018-04-30 10:38:26 +0000

    aa

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,4 @@
 class Artist < ApplicationRecord
   has_many :items
+  validates :artist_name, presence: true
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,4 @@
 class Song < ApplicationRecord
   belongs_to :disc
+  validates :song_name, presence: true
 end
